f30920b7e9c677d1ac2123d3f5233cac8222f317
Joyel <joyelmathew400@gmail.com>
2025-07-09 14:59:15 +0530

    updated the data

--- next-app/src/app/prediction/[name]/page.tsx
+++ next-app/src/app/prediction/[name]/page.tsx
@@ -1,25 +1,25 @@
-const getPredictedAge = async(name: string) => {
+const getPredictedAge = async (name: string) => {
   const res = await fetch(`https://api.agify.io/?name=${name}`);
   return res.json()
 };
 
-const getPredictedGender = async (name:string) => {
+const getPredictedGender = async (name: string) => {
   const res = await fetch(`https://api.genderize.io/?name=${name}`)
   return res.json();
 };
 
-const getPredictedCountry = async (name:string) => {
+const getPredictedCountry = async (name: string) => {
   const res = await fetch(`https://api.nationalize.io/?name=${name}`)
   return res.json();
 };
 
 
 
-interface Params{
-  params: {name: string};
+interface Params {
+  params: { name: string };
 }
 
-export default async function Page({params}: Params) {
+export default async function Page({ params }: Params) {
   const ageData = getPredictedAge(params.name);
   const genderData = getPredictedGender(params.name);
   const countryData = getPredictedCountry(params.name);
@@ -28,15 +28,19 @@ export default async function Page({params}: Params) {
   const [age, gender, country] = await Promise.all([ageData, genderData, countryData]);
 
   return (
-  <div>
-    <div>
-      <div>Personal Info</div>
-      <div>Age: {age?.age}</div>
-      <div>Gender: {gender?.gender}</div>
-      <div>Country: {country?.country[0]?.country_id}</div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          Personal Info
+        </div>
+        <div className="text-black">
+          <div>Age: {age?.age}</div>
+          <div>Gender: {gender?.gender}</div>
+          <div>Country: {country?.country[0]?.country_id}</div>
+        </div>
+      </div>
     </div>
-  </div>
-        
+
   );
 }
 
